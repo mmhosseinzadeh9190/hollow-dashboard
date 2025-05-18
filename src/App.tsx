190a4981ca6bccb0634b8f3aab0f925c59541bd4
Mohammad Mahdi Hosseinzadeh <mohammadmahdihosseinzadeh68@gmail.com>
2025-05-18 23:21:22 +0330

    refactor: remove ReactQueryDevtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Dashboard from "./pages/Dashboard";
 import Messages from "./pages/Messages";
@@ -32,8 +31,6 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
-
       <BrowserRouter>
         <Routes>
           <Route
